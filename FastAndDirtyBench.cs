c79bc9e80408eeca9a061df2845cfbe14fb3a0b6
Jack Casey <jackbonjour@gmail.com>
2019-08-07 22:56:06 -0700

    Multiple improvements

--- FastAndDirtyBench.cs
+++ FastAndDirtyBench.cs
@@ -1,68 +1,75 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace FastAndDirtyBench
 {
-    // TODO: Add comments 
-    // TODO: Run everything async to minimize performance impact 
-    // TODO: Add license
-    // This is a work in progress :) 
-
     public class Benchmarker
     {
-        public bool Enabled;
-
-        private readonly string OutputFile;
         private Dictionary<string, BenchmarkItem> BenchmarkItemStore;
 
-        public Benchmarker(string outputFile)
+        public Benchmarker()
         {
-            if (!File.Exists(outputFile)) File.Create(outputFile);
-
-            OutputFile = outputFile;
             BenchmarkItemStore = new Dictionary<string, BenchmarkItem>();
+
+            // Initialize dictionary to reduce possible latancy
+            BenchmarkItemStore.Add("Initialization", new BenchmarkItem
+            {
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now,
+                Completed = true,
+            });
+
+            BenchmarkItemStore.Remove("Initialization");
         }
 
         public void Start(string name)
         {
-            BenchmarkItemStore[name] = new BenchmarkItem
+            DateTime currentTime = DateTime.Now;
+
+            BenchmarkItemStore.Add(name, new BenchmarkItem
             {
-                Name = name,
-                StartTime = DateTime.Now,
-            };
+                StartTime = currentTime
+            });
         }
 
-        public void Finish(string name)
+        public void Stop(string name)
         {
-            BenchmarkItemStore[name].EndTime = DateTime.Now;
+            DateTime currentTime = DateTime.Now;
+
+            BenchmarkItemStore[name].EndTime = currentTime;
             BenchmarkItemStore[name].Completed = true;
         }
 
-        public void Output()
+        public void Output(string outputFile)
         {
-            using (StreamWriter sw = new StreamWriter(OutputFile, true))
-            {
-                sw.WriteLine();
-                sw.WriteLine("Output created at:");
-                sw.WriteLine(DateTime.Now.ToString("F"));
-                sw.WriteLine();
-                sw.WriteLine("~ ~ ~");
+            if (!File.Exists(outputFile)) File.Create(outputFile);
 
-                foreach (KeyValuePair<string, BenchmarkItem> kv in BenchmarkItemStore)
+            int longestName = BenchmarkItemStore.Select(k => k.Key.Length).Max();
+
+            string stringFormat = $"{{0, -{longestName + 5}}}{{1, -20}}{{2, -20}}"; // Looks ugly, but works
+
+            using (StreamWriter sw = new StreamWriter(outputFile, true))
+            {
+                sw.WriteLine($"Output created at: {DateTime.Now.ToString("F")}");
+                sw.WriteLine();
+                sw.WriteLine(String.Format(stringFormat, "Name:", "Time:", "Time (ms):"));
+
+                foreach (KeyValuePair<string, BenchmarkItem> kv in BenchmarkItemStore.OrderBy(kv => kv.Key))
                 {
                     if (kv.Value.Completed)
                     {
                         TimeSpan interval = kv.Value.EndTime - kv.Value.StartTime;
-                        sw.WriteLine($"{kv.Key} completed in: {interval.TotalMilliseconds}ms");
+                        sw.WriteLine(String.Format(stringFormat, kv.Key, interval.ToString(), interval.TotalMilliseconds));
                     }
                     else
                     {
-                        sw.WriteLine($"{kv.Key} did not complete");
+                        sw.WriteLine(String.Format(stringFormat, kv.Key, "Did Not Complete", "---"));
                     }
                 }
 
-                sw.WriteLine("~ ~ ~");
+                sw.WriteLine();
                 sw.WriteLine();
             }
         }
@@ -70,7 +77,6 @@ namespace FastAndDirtyBench
 
     public class BenchmarkItem
     {
-        public string Name { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public bool Completed { get; set; } = false;
